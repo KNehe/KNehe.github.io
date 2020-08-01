2658e7a70587e787bb8dbab762f45685156f8bb9
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2020-08-01 20:17:21 +0300

    Updates

---
+++ precache-manifest.9d7a14181bdb2050c0c9d8661630fe93.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86c40efa07df7ffadb4cd1a9e9b9c83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10226852433000600c67",
+    "url": "/static/css/main.145a3702.chunk.css"
+  },
+  {
+    "revision": "24210f906fc6569eb765",
+    "url": "/static/js/2.ec87cf12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ec87cf12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10226852433000600c67",
+    "url": "/static/js/main.09de1330.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "e83faa1592656e6e07f753d282db4831",
+    "url": "/static/media/ExpenseManager.e83faa15.png"
+  },
+  {
+    "revision": "95c666232fbb11ac47f3031e9e7d9aed",
+    "url": "/static/media/devices.95c66623.svg"
+  },
+  {
+    "revision": "b3346fe586668769b5fa134da693f22e",
+    "url": "/static/media/facebook.b3346fe5.svg"
+  },
+  {
+    "revision": "9790b6f3d00c5e677a26f21e9dd9856f",
+    "url": "/static/media/github.9790b6f3.svg"
+  },
+  {
+    "revision": "b35f3063a88288747aa94a9c363a4e1f",
+    "url": "/static/media/instagram.b35f3063.svg"
+  },
+  {
+    "revision": "cbeb62855fd56bf53a8081a2f167cc1f",
+    "url": "/static/media/linkedin-logo.cbeb6285.svg"
+  },
+  {
+    "revision": "b018dd4ee2594a6b20da9b88e21dd5e5",
+    "url": "/static/media/mysql.b018dd4e.svg"
+  },
+  {
+    "revision": "4e10752cfea51cf8f3fe7e12ed7e4947",
+    "url": "/static/media/nodejs-logo.4e10752c.png"
+  },
+  {
+    "revision": "fb0fd1d8bae789555a8710010cb29142",
+    "url": "/static/media/sharenews.fb0fd1d8.PNG"
+  },
+  {
+    "revision": "539d308c4f0064736a0d846381fab72e",
+    "url": "/static/media/spring.539d308c.png"
+  },
+  {
+    "revision": "d1e9f14f6fd4940ae99293b2e87a123c",
+    "url": "/static/media/tuulye.d1e9f14f.PNG"
+  },
+  {
+    "revision": "49224b0e53aec159950c88ac30caaa03",
+    "url": "/static/media/twitter.49224b0e.svg"
+  }
+]);
