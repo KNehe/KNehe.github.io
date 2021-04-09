9d6254ee151826becbc7000f724b0742476bd8af
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-04-09 22:40:49 +0300

    Modified about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Styles from './About.module.scss';
+import GitHubImg from '../../Assets/coding.svg'
 
 const about = () =>{
 
@@ -9,16 +10,14 @@ const about = () =>{
 
                     <div className={Styles.AboutDescription}>
                         <h4>About</h4>
-                        <div className={Styles.Text}>
-                        <p> I'm a self motivated person who is always learning, growing, improving my skills and making sure
-                            that i learn any new technology that will make me improve my services.
-                            I chose software development career because with the skills acquired its possible to solve problems and make
-                            the world a better place.
-                        I love bringing to life your ideas, goals, and dreams, nothing makes me happier.
-                        Over the years, developing software has continued to be  one of my top passsions. I've 
-                            been doing it for more than 3years. I have created APIs, web and mobile apps.
-                            
-                        </p>
+                        <div className={Styles.DescGroup}>
+                            <img src={GitHubImg} alt='' />
+                            <p  className={Styles.Text}> I'm a self motivated person who is always learning, growing, improving my skills and making sure
+                                that i learn any new technology that will make me improve my services.
+                                I chose software development because with the skills acquired its possible to solve problems and make
+                                the world a better place.
+                            I love bringing to life your ideas, goals, and dreams, nothing makes me happier.                                
+                            </p>
                         </div>                   
                     </div>                 
 
