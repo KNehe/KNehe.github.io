6caebbe49ff30473bcde26cfb12a1e3cf0c79769
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-04-12 18:49:51 +0300

    Fix: remove undefined packages,unused imports

--- src/Components/Project/project.js
+++ src/Components/Project/project.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {  faChevronDown , faLink} from '@fortawesome/free-solid-svg-icons';
 const project = props =>{
 
     return(
